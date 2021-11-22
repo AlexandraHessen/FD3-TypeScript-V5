2e22f42b7c710f95c17bfbd536dff4898e3dc186
AlexandraHessen <xl_energy@mail.ru>
2021-11-22 21:50:04 +0300

    type obj

--- A_TS0-TS1/TS070_types_hash/app.js
+++ A_TS0-TS1/TS070_types_hash/app.js
@@ -1,11 +1,9 @@
-var friend:{fam:string,im:string,otch:string,age:number}
-    ={fam:"Иванов",im:"Иван",otch:"Иванович",age:25};
+var friend = { fam: "Иванов", im: "Иван", otch: "Иванович", age: 25 };
 // только 4 ключа больше нельзя добавлять
 console.log(friend);
-
-
-var friendKey:keyof {fam:string,im:string,otch:string,age:number};
+var friendKey;
 // friendKey:keyof означает что переменная может быть только одним из ключей
-friendKey="fam";
+friendKey = "fam";
 // friendKey="fam2"; // ошибка
-console.log(friendKey) //fam
+console.log(friendKey);
+//# sourceMappingURL=app.js.map
